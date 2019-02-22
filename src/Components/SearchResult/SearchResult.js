323d8c621c3539ad84d65a32fc4f261ffcd0ce0a
zoltan_almodo <zoltanalmodo@gmail.com>
2019-02-22 06:27:08 +0000

    READY =search result interactive style READY = NEXT basic data flow

--- src/Components/SearchResult/SearchResult.js
+++ src/Components/SearchResult/SearchResult.js
@@ -5,7 +5,7 @@ import searchresult_image from './purpose-group-searchresult.png'
 
 export default function SearchResult() {
     return (
-        <div className='SearchResult'>
+        <div className='SearchResultPassive' >
             <img src={searchresult_image}
                 className="purpose-group-searchresult-image"
                 alt="purpose-group-searchresult" />
--- src/Components/SearchResultsPanel/SearchResultsPanel.js
+++ src/Components/SearchResultsPanel/SearchResultsPanel.js
@@ -1,14 +1,93 @@
 import React from 'react';
 
-import SearchResult from '../SearchResult/SearchResult'
+import SearchResultText from '../SearchResultText/SearchResultText'
+import searchresult_image from './purpose-group-searchresult.png'
 
 
-export default function SearchResultPanel() {
-    return (
-        <div className='SearchResultPanel'>
-            <SearchResult />
-            <SearchResult />
-            <SearchResult />
-        </div>
-    )
-}
+// export default function SearchResultPanel() {
+//     return (
+//         <div className='SearchResultPanel'>
+//             <SearchResult />
+//             <SearchResult />
+//             <SearchResult />
+//         </div>
+//     )
+// }
+
+
+class SearchResultsPanel extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+          Button_1: true,
+          Button_2: false,
+          Button_3: false,
+        }
+    }
+  
+    changeColor_1(){
+       this.setState({
+         Button_1: true,
+         Button_2: false,
+         Button_3: false,
+        })
+    }
+    changeColor_2(){
+      this.setState({
+        Button_1: false,
+        Button_2: true,
+        Button_3: false,
+       })
+    }
+    changeColor_3(){
+      this.setState({
+        Button_1: false,
+        Button_2: false,
+        Button_3: true,
+       })
+    }
+   
+  
+    
+    render(){
+        let Button_1_class = this.state.Button_1 ?
+            "SearchResultActive" :
+            "SearchResultPassive"
+        ;
+        let Button_2_class = this.state.Button_2 ?
+            "SearchResultActive" :
+            "SearchResultPassive"
+        ;
+        let Button_3_class = this.state.Button_3 ?
+            "SearchResultActive" :
+            "SearchResultPassive"
+        ;
+  
+        return (
+          <div className='SearchResultPanel'>
+
+            <div className={Button_1_class} onClick={this.changeColor_1.bind(this)}>
+                <img src={searchresult_image}
+                    className="purpose-group-searchresult-image"
+                    alt="purpose-group-searchresult" />
+                <SearchResultText />
+            </div>
+            <div className={Button_2_class} onClick={this.changeColor_2.bind(this)}>
+                <img src={searchresult_image}
+                    className="purpose-group-searchresult-image"
+                    alt="purpose-group-searchresult" />
+                <SearchResultText />
+            </div>
+            <div className={Button_3_class} onClick={this.changeColor_3.bind(this)}>
+                <img src={searchresult_image}
+                    className="purpose-group-searchresult-image"
+                    alt="purpose-group-searchresult" />
+                <SearchResultText />
+             </div>
+  
+          </div>
+        )
+    }
+  }
+  
+  export default SearchResultsPanel;
