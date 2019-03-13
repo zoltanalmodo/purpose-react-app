e7f3c458ae03f2a66632d9b959ce5e5a2c8ae840
zoltan_almodo <zoltanalmodo@gmail.com>
2019-03-13 00:02:51 +0000

    READY = file re-structuring = Next adding interaction and state changes

--- src/Containers/FloorPlanContainer/FloorPlanContainer.js
+++ src/Containers/FloorPlanContainer/FloorPlanContainer.js
@@ -8,7 +8,7 @@ class App extends Component {
     render() {
         return (
             <div className='FloorPlanContainer'>
-                <FloorPlanPanelTitle title = ''/>
+                <FloorPlanPanelTitle title = 'Title'/>
                 <FloorPlanPanel />
                 <FloorPlanDrawing />
             </div>
