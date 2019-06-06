7649b38e04e663380cde3b26b0c8f29a147e838c
zoltan_almodo <zoltanalmodo@gmail.com>
2019-06-06 19:19:10 +0200

    x

--- src/Components/Home/HomeGallery/HomeGallery.js
+++ src/Components/Home/HomeGallery/HomeGallery.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import AliceCarousel from 'react-alice-carousel/lib/react-alice-carousel.js'
+import AliceCarousel from 'react-alice-carousel'
 import 'react-alice-carousel/lib/alice-carousel.css'
 
 import image_001 from './images/home-slider-001.jpg';
